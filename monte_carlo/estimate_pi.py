43428b9c9438e8a0b2de37e5619b47cd26af020d
sully <sully@sullivandigitalconsulting.com>
2024-09-04 21:11:54 +0000

    Add progress inidicators to pi estimate monte carlo sim

--- monte_carlo/estimate_pi.py
+++ monte_carlo/estimate_pi.py
@@ -9,16 +9,25 @@ def estimate_pi(num_samples):
             points_inside_circle += 1
         print_progress(num_samples, i)
 
+    clear_progress()
     pi_estimate = 4 * points_inside_circle / num_samples
     return pi_estimate
 
-def print_progress(num_samples, current_iteration):
-    max_indicators = 10
-    indicator_period = num_samples / max_indicators
-    if current_iteration % indicator_period == 0:
-        print(".")
+def clear_progress():
+    print(" " * 80, end="\r")
 
-sample_sizes = np.geomspace(1, 1000000, num=10, dtype=int)
+def print_progress(num_samples, current_iteration):
+    max_indicators = 50
+    indicator_period = int(num_samples / max_indicators) + 1
+    if current_iteration % indicator_period == 0:
+        periods = current_iteration / indicator_period
+        percent = int(current_iteration / num_samples * 100)
+        progress = f"Sample size: {num_samples}. {percent}% "
+        for _ in range(int(periods)):
+            progress += "."
+        print(progress, end="\r")
+
+sample_sizes = np.geomspace(1, 3000000, num=15, dtype=int)
 for size in sample_sizes:
     estimate = estimate_pi(size)
     print(f"Sample size: {size}. Pi estimate: {estimate:.5f}")
