e83b7b923b5336e8803b2ae7a3e3015b637c6b82
sully <sully@sullivandigitalconsulting.com>
2024-09-05 02:09:26 +0000

    Added race condition to multiprocessing examples

--- concurrency/concurrent_futures_locks.py
+++ concurrency/concurrent_futures_locks.py
@@ -1,21 +1,25 @@
 import concurrent.futures
 import multiprocessing
 
-total_mp = multiprocessing.Value('i', 0)
+total_shared_locked = multiprocessing.Value('i', 0)
+total_shared = multiprocessing.Value('i', 0)
 total = 0
 
 def work(item):
     global total 
     result = item * item
     total += result
-    with total_mp.get_lock():
-        total_mp.value += result
+    total_shared.value += result
+    with total_shared_locked.get_lock():
+        total_shared_locked.value += result
 
 def main():
-    items = range(10000)
+    items = range(1000)
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        list(executor.map(work, items))
-    print(f"total_mp: {total_mp.value}")
+        executor.map(work, items)
+    print(f"total_shared_locked {total_shared_locked.value}")
+    # This will be slightly different every run since we will be loosing total updates to race conditions
+    print(f"total_shared: {total_shared.value}")
     # This will be zero because total is not actually shared in ProcessPoolExecutor.
     print(f"total: {total}")
 
